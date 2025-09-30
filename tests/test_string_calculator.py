1ff059777a3a483cd7ba8303b4cef24ddec60394
Greninja49 <hadiskhanofficial@gmail.com>
2025-09-30 19:26:23 +0530

    test: add test for custom delimiter syntax

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -12,3 +12,6 @@ def test_two_numbers_comma_separated():
 
 def test_newline_between_numbers():
     assert add("1\n2,3") == 6
+
+def test_custom_single_char_delimiter():
+    assert add("//;\n1;2") == 3
